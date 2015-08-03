515d97e52c72bb3b98c44085bfafa58235c8978f
Proxyfer <ganjablin@gmail.com>
2015-08-03 10:07:05 +0300

    Test work

---
+++ Cash Machine/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Cash_Machine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CASH OUT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(216, 262);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.listBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Initialize system..."});
+            this.listBox1.Location = new System.Drawing.Point(54, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(414, 225);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(443, 330);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(53, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "RESET";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(89, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Required amount: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(497, 353);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Cash Machine/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Cash_Machine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int stroke = new int();
+        public int all = 0;
+        public string[] values = new string[5] {"$1=", "$5=", "$10=", "$20=", "$100="};
+        public int[] cash = new int[5];
+                                    /*  0 = 1$
+                                        1 = 5$
+                                        2 = 10$
+                                        3 = 20$
+                                        4 = 100$  */
+
+        public void Cash_Show(int index)
+        {
+            switch (index)
+            {
+                case 1:
+                    listBox1.Items.Add("Inventory:");
+                    for (int i = 0; i < 5; i++) listBox1.Items.Add(values[i] + cash[i]);
+                    /*
+                     * cash[0]=value;
+                     * cash[1]=value;
+                     * cash[2]=value;
+                     * cash[3]=value;
+                     * cash[4]=value; 
+                     */
+                      
+                    all = cash[0] + cash[1] * 5 + cash[2] * 10 + cash[3] * 20 + cash[4] * 100;
+                    break;                    
+                case 2:
+                    listBox1.Items.Add("You enter invalid bet, all bets must be in increments of $1 and more than 0"); 
+                    break;
+                case 3: 
+                    listBox1.Items.Add("Theare not enough money in ATM, please enter another value");
+                    break;
+            }
+            
+            textBox1.Text = "";
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            listBox1.SelectedItem = null;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Cash_out=0;
+            try
+            {
+                Cash_out = Convert.ToInt32(textBox1.Text);
+            }
+            catch
+            {
+            }
+
+            if (Cash_out <= 0)
+                Cash_Show(2);
+            else if (Cash_out > all)
+                Cash_Show(3);
+            else if (Cash_out == all)
+            {
+                for (int i = 0; i < 5; i++) cash[i] = 0;
+                Cash_Show(1);
+            }
+            else
+            {
+                int[] additional_cash = new int[5];
+                Array.Copy(cash,additional_cash, 5);
+                while (Cash_out >= 1)
+                {
+                    while ((Cash_out >= 5)&(additional_cash[1] > 0))
+                    {
+                        while ((Cash_out >= 10)&(additional_cash[2] > 0))
+                        {
+                            while ((Cash_out >= 20) & (additional_cash[3] > 0))
+                            {
+                                while ((Cash_out >= 100) & (additional_cash[4] > 0))
+                                {
+                                        additional_cash[4] -= 1;
+                                        Cash_out -= 100;
+                                        
+                                }
+                                if (Cash_out >= 20)
+                                {
+                                    additional_cash[3] -= 1;
+                                    Cash_out -= 20;
+                                }
+                               
+                            }
+                            if (Cash_out >= 10)
+                            {
+                                additional_cash[2] -= 1;
+                                Cash_out -= 10;
+                            }
+                         
+                        }
+                        if (Cash_out >= 5)
+                        {
+                            additional_cash[1] -= 1;
+                            Cash_out -= 5;
+                        }
+                                               
+                    }
+                    if (Cash_out >= 1)
+                    {
+                        additional_cash[0] -= 1;
+                        Cash_out -= 1;
+                    }
+                }
+
+                if (additional_cash[0] < 0) Cash_Show(3);
+                else
+                {
+                    Array.Copy(additional_cash, cash, 5);
+                    Cash_Show(1);
+                }
+                
+            }
+                        
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)  cash[i] = 10;
+            Cash_Show(1); 
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2_Click(sender, e);
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) button1_Click(sender, e);
+        }
+    }
+}
